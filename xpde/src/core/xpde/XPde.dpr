9ac5e613a087091b6d2f89f0750b86276bef21d9
ttm <ttm>
2002-10-21 21:15:44 +0000

    Added task manager

--- xpde/src/core/xpde/XPde.dpr
+++ xpde/src/core/xpde/XPde.dpr
@@ -1,21 +1,11 @@
 program XPde;
 
 uses
-  QForms,
-  uXPStyle,
-  main in 'main.pas' {MainForm},
-  uCreateShortcut in 'uCreateShortcut.pas' {CreateShortcutDlg},
-  uLNKProperties in 'uLNKProperties.pas' {LNKPropertiesDlg},
-  uChangeIcon in 'uChangeIcon.pas' {ChangeIconDlg},
-  uOpenWith in 'uOpenWith.pas' {OpenWithDlg},
-  uXPAPI_imp in 'uXPAPI_imp.pas';
+  QForms;
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  SetXPStyle(application);  
-  Application.CreateForm(TMainForm, MainForm);
-  Application.CreateForm(TOpenWithDlg, OpenWithDlg);
   Application.Run;
 end.

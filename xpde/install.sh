c9eb0a95dc7feffe7b90abb4453c30f73ebaf039
ttm <ttm>
2003-01-25 18:10:45 +0000

    Installation script for developers

---
+++ xpde/install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+echo "Remember you must execute this program as root!!!";
+
+mkdir "/opt"
+
+mkdir "/opt/xpde"
+mkdir "/opt/xpde/bin"
+mkdir "/opt/xpde/bin/apps"
+mkdir "/opt/xpde/bin/applets"
+
+mkdir "/opt/xpde/share"
+mkdir "/opt/xpde/share/apps"
+mkdir "/opt/xpde/share/applets"
+mkdir "/opt/xpde/share/doc"
+mkdir "/opt/xpde/share/fonts"
+mkdir "/opt/xpde/share/icons"
+
+mkdir "/opt/xpde/themes"
+
+cp -r themes/default /opt/xpde/themes
+cp -r doc/* /opt/xpde/share/doc
+cp *.so* /opt/xpde/bin
+cp startXPde /opt/xpde/bin
+cp XPde /opt/xpde/bin
+cp XPwm /opt/xpde/bin
+cp stub.sh /opt/xpde/bin
+
+cp DateTimeProps /opt/xpde/bin/applets
+cp appexec /opt/xpde/bin/applets
+cp networkstatus /opt/xpde/bin/applets
+cp xpsu /opt/xpde/bin/applets
+
+cp taskmanager /opt/xpde/bin/apps
+
+echo "Installation completed";

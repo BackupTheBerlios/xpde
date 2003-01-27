8babe7c54dc7b3e05bf4394195ffe52bbc484c57
ttm <ttm>
2003-01-27 20:32:32 +0000

    Latest additions to automate builds

---
+++ xpde/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Remember you must execute this program as user, not as root!!!";
+
+cp -r defaultdesktop ~/.xpde
+cp xinitrcDEFAULT ~
+
+echo "Installation completed, read the INSTALL file to know how to setup XPde as your default environment";

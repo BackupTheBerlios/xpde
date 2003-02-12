ea8a69ec174751d370208b7df7ee047e06e9cb3b
ttm <ttm>
2003-02-12 20:17:00 +0000

    Resource strings

---
+++ xpde/src/core/xpde/uXPdeconsts.pas
@@ -0,0 +1,23 @@
+unit uXPdeconsts;
+
+interface
+
+resourcestring
+    sMyDocuments= 'My Documents';
+    sMyComputer= 'My Computer';
+    sMyHome='My Home';
+    sMyNetworkPlaces='My Network Places';
+    sRecycleBin='Recycle Bin';
+    sNewFolder='New Folder (%d)';
+    sSelectADirectory= 'Select a directory';
+
+    sNextStep='&Next >';
+    sCreateShortcut='Create Shortcut';
+    sFinishWizard='&Finish';
+    sSelectATitleForTheProgram='Select a Title for the Program';
+
+    sShortcut='Shortcut';
+
+implementation
+
+end.

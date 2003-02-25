2e3b35cb2df0a66785d06e541ee8af5c2998f00a
ttm <ttm>
2003-02-25 15:06:44 +0000

    GPL notice and initial clipboard support

--- xpde/src/apps/fileexplorer/uExplorerAPI.pas
+++ xpde/src/apps/fileexplorer/uExplorerAPI.pas
@@ -1,3 +1,25 @@
+{ *************************************************************************** }
+{                                                                             }
+{ This file is part of the XPde project                                       }
+{                                                                             }
+{ Copyright (c) 2002 José León Serna <ttm@xpde.com>                           }
+{                                                                             }
+{ This program is free software; you can redistribute it and/or               }
+{ modify it under the terms of the GNU General Public                         }
+{ License as published by the Free Software Foundation; either                }
+{ version 2 of the License, or (at your option) any later version.            }
+{                                                                             }
+{ This program is distributed in the hope that it will be useful,             }
+{ but WITHOUT ANY WARRANTY; without even the implied warranty of              }
+{ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           }
+{ General Public License for more details.                                    }
+{                                                                             }
+{ You should have received a copy of the GNU General Public License           }
+{ along with this program; see the file COPYING.  If not, write to            }
+{ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,            }
+{ Boston, MA 02111-1307, USA.                                                 }
+{                                                                             }
+{ *************************************************************************** }
 unit uExplorerAPI;
 
 interface
@@ -12,7 +34,11 @@ type
         function getChildren: TInterfaceList;
         function getDisplayName: string;
         procedure getColumns(const columns:TStrings);
+        procedure setNode(node:TObject);
+        function getNode:TObject; 
         procedure getColumnData(const columns:TStrings);
+        procedure doubleClick;
+        function getUniqueID:string;
         procedure getStatusData(const status:TStrings);
         procedure getVerbItems(const verbs:TStrings);
         procedure executeVerb(const verb:integer);
@@ -32,6 +58,11 @@ type
         function registerImage(const bmp: TBitmap):integer;
         function getRootItems: TInterfaceList;
         function getImageList: TImageList;
+        function getClipboard:TStringList;
+        function ClipboardEmpty:boolean;
+        procedure clearclipboard;
+        procedure copytoclipboard(const item:string); overload;
+        procedure copytoclipboard(const items:TStrings); overload;
     end;
 
 var
--- xpde/src/apps/fileexplorer/uXPExplorer.pas
+++ xpde/src/apps/fileexplorer/uXPExplorer.pas
@@ -1,3 +1,25 @@
+{ *************************************************************************** }
+{                                                                             }
+{ This file is part of the XPde project                                       }
+{                                                                             }
+{ Copyright (c) 2002 José León Serna <ttm@xpde.com>                           }
+{                                                                             }
+{ This program is free software; you can redistribute it and/or               }
+{ modify it under the terms of the GNU General Public                         }
+{ License as published by the Free Software Foundation; either                }
+{ version 2 of the License, or (at your option) any later version.            }
+{                                                                             }
+{ This program is distributed in the hope that it will be useful,             }
+{ but WITHOUT ANY WARRANTY; without even the implied warranty of              }
+{ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           }
+{ General Public License for more details.                                    }
+{                                                                             }
+{ You should have received a copy of the GNU General Public License           }
+{ along with this program; see the file COPYING.  If not, write to            }
+{ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,            }
+{ Boston, MA 02111-1307, USA.                                                 }
+{                                                                             }
+{ *************************************************************************** }
 unit uXPExplorer;
 
 interface
@@ -10,7 +32,13 @@ type
     private
         roots: TInterfaceList;
         images: TImageList;
+        clipboard: TStringList;
     public
+        function ClipboardEmpty:boolean;
+        function getClipboard:TStringList;
+        procedure clearclipboard;
+        procedure copytoclipboard(const item:string); overload;
+        procedure copytoclipboard(const items:TStrings); overload;        
         function registerImage(const bmp: TBitmap):integer;
         procedure registerRootItem(item:IXPVirtualFile);
         function getImageList: TImageList;
@@ -26,17 +54,46 @@ uses
 
 { TXPExplorer }
 
+procedure TXPExplorer.clearclipboard;
+begin
+    clipboard.clear;
+end;
+
+procedure TXPExplorer.copytoclipboard(const item: string);
+begin
+    clearclipboard;
+    clipboard.add(item);
+end;
+
+function TXPExplorer.ClipboardEmpty: boolean;
+begin
+    result:=(clipboard.count=0);
+end;
+
+procedure TXPExplorer.copytoclipboard(const items: TStrings);
+begin
+    clearclipboard;
+    clipboard.addstrings(items);
+end;
+
 constructor TXPExplorer.Create;
 begin
     roots:=TInterfaceList.create;
     images:=TImageList.create(nil);
+    clipboard:=TStringList.create;
 end;
 
 destructor TXPExplorer.Destroy;
 begin
+    clipboard.free;
     images.free;
     roots.free;
     inherited;
+end;
+
+function TXPExplorer.getClipboard: TStringList;
+begin
+    result:=clipboard;
 end;
 
 function TXPExplorer.getImageList: TImageList;

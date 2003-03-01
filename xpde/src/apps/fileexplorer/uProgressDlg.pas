a5dc8dd315e4afa68c3c6cab8e64cf537a100f27
ttm <ttm>
2003-03-01 20:16:37 +0000

    Isolation from graphics

--- xpde/src/apps/fileexplorer/uProgressDlg.pas
+++ xpde/src/apps/fileexplorer/uProgressDlg.pas
@@ -1,3 +1,25 @@
+{ *************************************************************************** }
+{                                                                             }
+{ This file is part of the XPde project                                       }
+{                                                                             }
+{ Copyright (c) 2002 José León Serna <ttm@xpde.com>                           }
+{                                                                             }
+{ This program is free software; you can redistribute it and/or               }
+{ modify it under the terms of the GNU General Public                         }
+{ License as published by the Free Software Foundation; either                }
+{ version 2 of the License, or (at your option) any later version.            }
+{                                                                             }
+{ This program is distributed in the hope that it will be useful,             }
+{ but WITHOUT ANY WARRANTY; without even the implied warranty of              }
+{ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           }
+{ General Public License for more details.                                    }
+{                                                                             }
+{ You should have received a copy of the GNU General Public License           }
+{ along with this program; see the file COPYING.  If not, write to            }
+{ the Free Software Foundation, Inc., 59 Temple Place - Suite 330,            }
+{ Boston, MA 02111-1307, USA.                                                 }
+{                                                                             }
+{ *************************************************************************** }
 unit uProgressDlg;
 
 interface
@@ -5,19 +27,20 @@ interface
 uses
   SysUtils, Types, Classes,
   Variants, QTypes, QGraphics,
-  QControls, QForms, QDialogs,
+  QControls, QForms, QDialogs, uXPAPI,
   QStdCtrls, QExtCtrls, QComCtrls,
   uQXPComCtrls;
 
 type
   TProgressDlg = class(TForm)
-    Image2: TImage;
+    imProgress: TImage;
     lbFile: TLabel;
     lbStatus: TLabel;
     pbProgress: TProgressBar;
     btnCancel: TButton;
     lbETA: TLabel;
     procedure btnCancelClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -47,6 +70,11 @@ end;
 procedure TProgressDlg.btnCancelClick(Sender: TObject);
 begin
     tag:=1;
+end;
+
+procedure TProgressDlg.FormCreate(Sender: TObject);
+begin
+    imProgress.Picture.LoadFromFile(XPAPI.getsysinfo(siMiscDir)+'copyprogress.png');
 end;
 
 end.

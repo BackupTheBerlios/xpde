56d5e55363d39fc41ae659ded3d0ac91e985c1ad
zeljko <zeljko>
2003-07-17 08:42:18 +0000

    System Properties applet is useable at the moment since it can provide a lot of hardware informations.

--- xpde/src/applets/systemproperties/systemproperties.dpr
+++ xpde/src/applets/systemproperties/systemproperties.dpr
@@ -3,7 +3,12 @@ program systemproperties;
 uses
   QForms,
   uXPStyle,
-  uSystemProperties in 'uSystemProperties.pas' {SystemPropertiesDlg};
+  uSystemProperties in 'uSystemProperties.pas' {SystemPropertiesDlg},
+  uDeviceManager in 'uDeviceManager.pas' {frmSystem},
+  uHWProperties in 'uHWProperties.pas' {frmProp},
+  uXPImageList in '../xpde/src/components/xpimagelist/uXPImageList.pas',
+  uXPListview in '../xpde/src/components/xplistview/uXPListview.pas',
+  SysProvider in '../sysprovider/SysProvider.pas';
 
 {$R *.res}
 

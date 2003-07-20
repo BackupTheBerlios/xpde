ff8ab495f346012459ffcd0e9a445d865b00341e
ttm <ttm>
2003-07-20 10:05:14 +0000

    Final GUI changes

--- xpde/src/applets/systemproperties/systemproperties.dpr
+++ xpde/src/applets/systemproperties/systemproperties.dpr
@@ -5,8 +5,7 @@ uses
   uXPStyle,
   uSystemProperties in 'uSystemProperties.pas' {SystemPropertiesDlg},
   uDeviceManager in 'uDeviceManager.pas' {frmSystem},
-  uHWProperties in 'uHWProperties.pas' {frmProp},
-  uTest in 'uTest.pas' {Form1};
+  uHWProperties in 'uHWProperties.pas' {frmProp};
 
 {$R *.res}
 
